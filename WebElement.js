b077950b93fe06d6f6a8e00a3281f04145644b18
Kshitij Nigam <nigamelastic@gmail.com>
2021-05-18 21:53:33 +0200

    Create WebElement.js

---
+++ WebElement.js
@@ -0,0 +1,47 @@
+
+function getElementById(element, idToFind) {  
+  var descendants = element.getDescendants();  
+  for(i in descendants) {
+    var elt = descendants[i].asElement();
+    if( elt !=null) {
+      var id = elt.getAttribute('id');
+      if( id !=null && id.getValue()== idToFind) return elt;    
+    }
+  }
+}
+
+function getElementsByClassName(element, classToFind) {  
+  var data = [];
+  var descendants = element.getDescendants();
+  descendants.push(element);  
+  for(i in descendants) {
+    var elt = descendants[i].asElement();
+    if(elt != null) {
+      var classes = elt.getAttribute('class');
+      if(classes != null) {
+        classes = classes.getValue();
+        if(classes == classToFind) data.push(elt);
+        else {
+          classes = classes.split(' ');
+          for(j in classes) {
+            if(classes[j] == classToFind) {
+              data.push(elt);
+              break;
+            }
+          }
+        }
+      }
+    }
+  }
+  return data;
+}
+
+function getElementsByTagName(element, tagName) {  
+  var data = [];
+  var descendants = element.getDescendants();  
+  for(i in descendants) {
+    var elt = descendants[i].asElement();     
+    if( elt !=null && elt.getName()== tagName) data.push(elt);      
+  }
+  return data;
+}
